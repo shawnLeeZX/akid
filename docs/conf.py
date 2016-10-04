811a0a80a0d41a947b667cc32161e23ecb92df42
Shuai Li <lishuai918@gmail.com>
2016-10-04 09:26:13 +0800

    Project directory restructured. Added a TODO item.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 print "Docs are in " + os.path.abspath('.')
 print "Parent folder: " + os.path.abspath('..')
