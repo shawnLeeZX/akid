8c8efded12ecbf2733b074a5f5c43cd05fc972bc
Shuai Li <lishuai918@gmail.com>
2017-04-27 09:48:24 +0800

    Added back forgotten commented out tests.

--- tests/test_kids.py
+++ tests/test_kids.py
@@ -19,32 +19,32 @@ class TestKid(AKidTestCase):
         loss = kid.practice()
         assert loss < 0.2
 
-    # def test_saver(self):
-        # brain = TestFactory.get_test_brain()
-        # source = TestFactory.get_test_feed_source()
-        # kid = TestFactory.get_test_kid(source, brain)
-        # kid.setup()
+    def test_saver(self):
+        brain = TestFactory.get_test_brain()
+        source = TestFactory.get_test_feed_source()
+        kid = TestFactory.get_test_kid(source, brain)
+        kid.setup()
 
-        # loss = kid.practice()
-        # assert loss < 0.2
+        loss = kid.practice()
+        assert loss < 0.2
 
-        # kid.restore_from_ckpt()
-        # loss = kid.validate()
-        # assert loss < 0.2
+        kid.restore_from_ckpt()
+        loss = kid.validate()
+        assert loss < 0.2
 
-    # def test_log_to_file_flag(self):
-        # brain = TestFactory.get_test_brain()
-        # source = TestFactory.get_test_feed_source()
-        # kid = Kid(
-            # FeedSensor(source_in=source, name='data'),
-            # brain,
-            # MomentumKongFu(),
-            # log_dir="log_test_kid",
-            # log_to_file=False,
-            # max_steps=900)
-        # kid.setup()
+    def test_log_to_file_flag(self):
+        brain = TestFactory.get_test_brain()
+        source = TestFactory.get_test_feed_source()
+        kid = Kid(
+            FeedSensor(source_in=source, name='data'),
+            brain,
+            MomentumKongFu(),
+            log_dir="log_test_kid",
+            log_to_file=False,
+            max_steps=900)
+        kid.setup()
 
-        # assert not os.path.exists(kid.log_dir + "/training.log)")
+        assert not os.path.exists(kid.log_dir + "/training.log)")
 
 
 if __name__ == "__main__":
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -12,124 +12,124 @@ from akid import LearningRateScheme
 
 
 class TestSource(AKidTestCase):
-    # def test_mnist_feed_source(self):
-    #     source = MNISTFeedSource(
-    #         name="MNIST_feed",
-    #         url='http://yann.lecun.com/exdb/mnist/',
-    #         num_train=50000,
-    #         num_val=5000,
-    #         scale=True)
+    def test_mnist_feed_source(self):
+        source = MNISTFeedSource(
+            name="MNIST_feed",
+            url='http://yann.lecun.com/exdb/mnist/',
+            num_train=50000,
+            num_val=5000,
+            scale=True)
 
-    #     source.forward()
-    #     imgs, labels = source.get_batch(1, True)
-    #     img = np.squeeze(imgs)
-    #     plt.imshow(img)
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        source.forward()
+        imgs, labels = source.get_batch(1, True)
+        img = np.squeeze(imgs)
+        plt.imshow(img)
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    #     imgs, labels = source.get_batch(1, False)
-    #     img = np.squeeze(imgs)
-    #     plt.imshow(img)
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        imgs, labels = source.get_batch(1, False)
+        img = np.squeeze(imgs)
+        plt.imshow(img)
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    # def test_rotated_mnist_feed_source(self):
-    #     source = RotatedMNISTFeedSource(
-    #         name="Rotated_MNIST_feed",
-    #         url='http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip',
-    #         work_dir=AKID_DATA_PATH + '/rotated_mnist',
-    #         num_train=12000,
-    #         num_val=50000,
-    #         scale=True)
+    def test_rotated_mnist_feed_source(self):
+        source = RotatedMNISTFeedSource(
+            name="Rotated_MNIST_feed",
+            url='http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip',
+            work_dir=AKID_DATA_PATH + '/rotated_mnist',
+            num_train=12000,
+            num_val=50000,
+            scale=True)
 
-    #     source.forward()
-    #     imgs, labels = source.get_batch(1, True)
-    #     img = np.squeeze(imgs)
-    #     plt.imshow(img)
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        source.forward()
+        imgs, labels = source.get_batch(1, True)
+        img = np.squeeze(imgs)
+        plt.imshow(img)
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    #     imgs, labels = source.get_batch(1, False)
-    #     img = np.squeeze(imgs)
-    #     plt.imshow(img)
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        imgs, labels = source.get_batch(1, False)
+        img = np.squeeze(imgs)
+        plt.imshow(img)
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    # def test_cifar10_feed_source(self):
-    #     # Just read a batch and plot channels of images to see.
-    #     source = Cifar10FeedSource(
-    #         name="CIFAR10",
-    #         url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
-    #         work_dir=AKID_DATA_PATH + '/cifar10',
-    #         num_train=50000,
-    #         num_val=10000)
+    def test_cifar10_feed_source(self):
+        # Just read a batch and plot channels of images to see.
+        source = Cifar10FeedSource(
+            name="CIFAR10",
+            url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
+            work_dir=AKID_DATA_PATH + '/cifar10',
+            num_train=50000,
+            num_val=10000)
 
-    #     source.forward()
-    #     imgs, labels = source.get_batch(1, True)
-    #     plt.imshow(imgs[0, ...])
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        source.forward()
+        imgs, labels = source.get_batch(1, True)
+        plt.imshow(imgs[0, ...])
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    #     imgs, labels = source.get_batch(1, False)
-    #     plt.imshow(imgs[0, ...])
-    #     print("The class label is {}.".format(labels[0]))
-    #     plt.show()
+        imgs, labels = source.get_batch(1, False)
+        plt.imshow(imgs[0, ...])
+        print("The class label is {}.".format(labels[0]))
+        plt.show()
 
-    # def test_cifar10_zca_feed_source(self):
-    #     # Just read a batch and plot channels of images to see.
-    #     source = Cifar10FeedSource(
-    #         name="CIFAR10",
-    #         url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
-    #         work_dir=AKID_DATA_PATH + '/cifar10',
-    #         use_zca=True,
-    #         num_train=50000,
-    #         num_val=10000)
+    def test_cifar10_zca_feed_source(self):
+        # Just read a batch and plot channels of images to see.
+        source = Cifar10FeedSource(
+            name="CIFAR10",
+            url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
+            work_dir=AKID_DATA_PATH + '/cifar10',
+            use_zca=True,
+            num_train=50000,
+            num_val=10000)
 
-    #     source.forward()
-    #     imgs, labels = source.get_batch(1, True)
-    #     for c in xrange(0, 3):
-    #         plt.imshow(imgs[0, ..., c])
-    #         print("The class label is {}.".format(labels[0]))
-    #         plt.show()
-    #     imgs, labels = source.get_batch(1, False)
-    #     for c in xrange(0, 3):
-    #         plt.imshow(imgs[0, ..., c])
-    #         print("The class label is {}.".format(labels[0]))
-    #         plt.show()
+        source.forward()
+        imgs, labels = source.get_batch(1, True)
+        for c in xrange(0, 3):
+            plt.imshow(imgs[0, ..., c])
+            print("The class label is {}.".format(labels[0]))
+            plt.show()
+        imgs, labels = source.get_batch(1, False)
+        for c in xrange(0, 3):
+            plt.imshow(imgs[0, ..., c])
+            print("The class label is {}.".format(labels[0]))
+            plt.show()
 
-    # def test_cifar10_zca_tf_source(self):
-    #     from akid.models.brains import VGGNet
-    #     from akid import IntegratedSensor, Kid, GradientDescentKongFu
-    #     source = Cifar10TFSource(
-    #         name="CIFAR10",
-    #         url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
-    #         work_dir=AKID_DATA_PATH + '/cifar10',
-    #         use_zca=True,
-    #         num_train=50000,
-    #         num_val=10000)
+    def test_cifar10_zca_tf_source(self):
+        from akid.models.brains import VGGNet
+        from akid import IntegratedSensor, Kid, GradientDescentKongFu
+        source = Cifar10TFSource(
+            name="CIFAR10",
+            url='http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz',
+            work_dir=AKID_DATA_PATH + '/cifar10',
+            use_zca=True,
+            num_train=50000,
+            num_val=10000)
 
-    #     sensor = IntegratedSensor(source_in=source,
-    #                               batch_size=128,
-    #                               val_batch_size=100,
-    #                               name='data')
+        sensor = IntegratedSensor(source_in=source,
+                                  batch_size=128,
+                                  val_batch_size=100,
+                                  name='data')
 
-    #     brain = VGGNet(padding="SAME", name="VGGNet")
-    #     kid = Kid(
-    #         sensor,
-    #         brain,
-    #         GradientDescentKongFu(
-    #             lr_scheme={
-    #                 "name": LearningRateScheme.exp_decay,
-    #                 "base_lr": 0.1,
-    #                 "decay_rate": 0.1,
-    #                 "num_batches_per_epoch": 391,
-    #                 "decay_epoch_num": 350}),
-    #         summary_on_val=True,
-    #         max_steps=1000)
-    #     kid.setup()
+        brain = VGGNet(padding="SAME", name="VGGNet")
+        kid = Kid(
+            sensor,
+            brain,
+            GradientDescentKongFu(
+                lr_scheme={
+                    "name": LearningRateScheme.exp_decay,
+                    "base_lr": 0.1,
+                    "decay_rate": 0.1,
+                    "num_batches_per_epoch": 391,
+                    "decay_epoch_num": 350}),
+            summary_on_val=True,
+            max_steps=1000)
+        kid.setup()
 
-    #     loss = kid.practice()
-    #     assert loss < 1.6
+        loss = kid.practice()
+        assert loss < 1.6
 
     def test_cifar100_zca_tf_source(self):
         from akid.models.brains import VGGNet
@@ -218,40 +218,40 @@ class TestSource(AKidTestCase):
         loss = kid.practice()
         assert loss < 7.3
 
-    # def test_imagenet_source(self):
-    #     # Ideally, this source is supposed to test against the read image and
-    #     # labels, however, for time's sake ... just check compilation and
-    #     # runtime errors for now.
-    #     from akid import ImagenetTFSource
-    #     source = ImagenetTFSource(
-    #         name="Imagenet",
-    #         url=None,
-    #         work_dir=AKID_DATA_PATH + "/small_imagenet",
-    #         num_train=84321,
-    #         num_val=3300)
-    #     source.forward()
+    def test_imagenet_source(self):
+        # Ideally, this source is supposed to test against the read image and
+        # labels, however, for time's sake ... just check compilation and
+        # runtime errors for now.
+        from akid import ImagenetTFSource
+        source = ImagenetTFSource(
+            name="Imagenet",
+            url=None,
+            work_dir=AKID_DATA_PATH + "/small_imagenet",
+            num_train=84321,
+            num_val=3300)
+        source.forward()
 
-    #     with tf.Session() as sess:
-    #         sess.run([source.training_datum,
-    #                   source.training_label,
-    #                   source.val_datum,
-    #                   source.val_label])
+        with tf.Session() as sess:
+            sess.run([source.training_datum,
+                      source.training_label,
+                      source.val_datum,
+                      source.val_label])
 
-    #     # Again just test for compilation and runtime error.
-    #     source = ImagenetTFSource(
-    #         has_super_label=False,
-    #         name="Imagenet",
-    #         url=None,
-    #         work_dir=AKID_DATA_PATH + "/imagenet",
-    #         num_train=1281167,
-    #         num_val=50000)
-    #     source.forward()
+        # Again just test for compilation and runtime error.
+        source = ImagenetTFSource(
+            has_super_label=False,
+            name="Imagenet",
+            url=None,
+            work_dir=AKID_DATA_PATH + "/imagenet",
+            num_train=1281167,
+            num_val=50000)
+        source.forward()
 
-    #     with tf.Session() as sess:
-    #         sess.run([source.training_datum,
-    #                   source.training_label,
-    #                   source.val_datum,
-    #                   source.val_label])
+        with tf.Session() as sess:
+            sess.run([source.training_datum,
+                      source.training_label,
+                      source.val_datum,
+                      source.val_label])
 
 if __name__ == "__main__":
     main()
